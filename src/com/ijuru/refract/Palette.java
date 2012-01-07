307671ca179c6726675f917f97f07163e1f3d875
Rowan Seymour <rowanseymour@gmail.com>
2012-01-07 23:14:34 +0000

    Added all palette presets from original refract

--- src/com/ijuru/refract/Palette.java
+++ src/com/ijuru/refract/Palette.java
@@ -41,6 +41,22 @@ public class Palette {
 			new int[] { 0x0D1C40, 0x46787A, 0xFFFF00 },
 			new float[] { 0.0f, 0.4f, 1.0f }
 		));
+		presets.put("rainbow", new Palette(
+			new int[] { 0xFF0000, 0xFF8000, 0xFFFF00, 0x00FF00, 0x00FFFF, 0x0080FF, 0x0000FF, 0xFF0000 },
+			new float[] { 0.0f, 0.143f, 0.286f, 0.429f, 0.571f, 0.714f, 0.857f, 1.0f }
+		));
+		presets.put("chrome", new Palette(
+			new int[] { 0x2989CC, 0xFFFFFF, 0x906A00, 0xD99F00, 0xFFFFFF },
+			new float[] { 0.0f, 0.49f, 0.5f, 0.625f, 1.0f }
+		));
+		presets.put("evening", new Palette(
+			new int[] { 0x002874, 0xFD7C00, 0x6F156C, 0xF9E600 },
+			new float[] { 0.0f, 0.333f, 0.666f, 1.0f }
+		));
+		presets.put("electric", new Palette(
+			new int[] { 0x000010, 0x1000F0, 0xFFFFFF },
+			new float[] { 0.0f, 0.3f, 1.0f }
+		));
 	}
 	
 	/**
